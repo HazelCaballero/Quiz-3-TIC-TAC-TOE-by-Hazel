f0f091167243b8e561c98a87b00cb0a104db554a
HazelCaballero <hazelcabaelleroelizondo@gmail.com>
2025-02-12 15:38:24 -0600

    Actualizacion de js, ya chekea si hay ganador y quien es, pero la logica de empate no funciona, estuve un buen tiempo en un bug que hacia que la pc jugara doble turno, ya solucionado y el boton de reinicio ya tiene funcionalidad para limpiar contenido despues de una partida y agregue un hover a las casillas, aun tengo que solucionar el que no pueda marcar una casilla que ya tenga contenido

--- quiz3ByHazelScript.js
+++ quiz3ByHazelScript.js
@@ -17,57 +17,78 @@ const boxes = [
     document.getElementById("cell6"),
     document.getElementById("cell7"),
     document.getElementById("cell8")
-]
+];
 
-    let moves = []
+    let moves = [];
     let jugadorT = "X";
     let gameOver = false;
 
-    //logre que se almacenen las movidas x, pero aun no aplico funcionalidad para que la pc marque "O"
-function playerMove(){
-    boxes.forEach(cell => {
-    cell.addEventListener("click", function() {
-        cell.textContent = "X";
-        moves.push(cell.id)
-        localStorage.setItem("storMoves", (moves));
-        setTimeout(() => {
-            randomMove()
-        }, 600);
-    });
-});
-}
+    //funcion movimiento of player
+    function playerMove() {
+        boxes.forEach(cell => 
+            { cell.addEventListener("click", function () {
+                    if (!gameOver) {
+                        cell.textContent = "X"; 
+                        moves.push(cell.id);
+                        localStorage.setItem("storMoves", JSON.stringify(moves)); 
+                        checkWinner(); 
+                        if (!gameOver) {
+                            setTimeout(() => { randomMove(); }, 600); 
+                        }
+                    }
+                });
+        });
+    }
 
-//filtro para saber cuales estan vacias
+//filtro para saber cuales estan empty
 function randomMove() {
     
-    let emptyBoxes = boxes.filter(box => box.textContent === "")
-    if(emptyBoxes.length === 0)
-        return;
+    let emptyBoxes = boxes.filter(box => box.textContent === "");
+    if(emptyBoxes.length === 0) return;
 
 //para que la pc escoja aleatoriamente uno de esos espacios vacios y ponga en su contenido "O"
     let pcPushORandom = Math.floor(Math.random()*emptyBoxes.length);
     emptyBoxes[pcPushORandom].textContent ="O";
     moves.push(emptyBoxes[pcPushORandom].id);
+    checkWinner();
 }
 
 
-// para comprobar ganador
+// variables con win
 const winnerCombinations = [
     [0,1,2], [3,4,5], [6,7,8],
     [0,3,6], [1,4,7], [2,5,8],
     [0,4,8], [2,4,6]
-]
+];
 
-// no se si voy bien pero lo voy a guardar asi antes de almuerzo para evitar perder informacion
+// no se si voy bien 
 function checkWinner() {
     for(let combination of winnerCombinations){
         const [a,b,c] = combination;
         if(boxes[a].textContent && boxes[a].textContent === boxes[b].textContent && 
             boxes[a].textContent ===boxes[c].textContent){
-                
+                winner.textContent = `¡${boxes[a].textContent}gana!`
+                gameOver = true;
+                }
             }
+            //intente solucionar la mala funcion de la logica de empate porque si tenia solo el,
+            //  moves.length === 9 y se ganaba en el ultimo movimiento decia empate = error logico
+            if(moves.length === 9 && boxes[a].textContent && boxes[a].textContent === 
+                boxes[b].textContent && boxes[a].textContent === boxes[c].textContent){
+                    winner.textContent = "¡Es un empate!";
+                    gameOver = true;
     }
 }
+
+//btnreiniciar
+btnRestart.addEventListener("click", function(){
+    boxes.forEach(cell => cell.textContent = "");
+    moves =[];
+    gameOver = false
+    winner.textContent ="";
+    localStorage.clear()
+}
+);
 
 playerMove()
 
